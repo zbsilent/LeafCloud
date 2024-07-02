4742da0ae83bea2060af259648f29acc67dd7cf9
zbsilent <zbsilent@gmail.com>
2024-07-03 02:02:43 +0800

    other: init

--- leaf-common/src/main/java/com/leaf/cloud/common/R.java
+++ leaf-common/src/main/java/com/leaf/cloud/common/R.java
@@ -5,56 +5,67 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 /**
+ * 通用响应类，用于封装API的响应数据。
+ *
  * @author zbsilent
  * @version 1.0
- * @description: TODO
- * @date 2024/7/2 下午11:03
+ * @date 2024/7/2
  */
 @Data
 @AllArgsConstructor
 @Accessors(chain = true)
 public class R<T> {
 
-    private int code;
-    private String msg;
-    private T data;
-    private Long timeStamp;
+    private int code; // 状态码
+    private String msg; // 消息提示
+    private T data; // 返回的数据
+    private Long timeStamp; // 时间戳
 
+    // 默认构造函数，初始化时间戳
     public R() {
         this.timeStamp = System.currentTimeMillis();
     }
 
-    // 成功返回
+    // 构造一个成功的响应，包含数据
     public R(T data) {
+        this(200, "success", data);
+    }
+
+    // 构造一个指定状态码、消息和数据的响应
+    public R(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
         this.data = data;
         this.timeStamp = System.currentTimeMillis();
-        this.code = 200;
-        this.msg = "success";
     }
 
+    // 静态方法创建成功的响应，包含数据
     public static <T> R<T> success(T data) {
-        return new R<T>().setCode(200).setMsg("success").setData(data);
+        return new R<>(200, "success", data);
     }
 
+    // 静态方法创建成功的空响应
     public static <T> R<T> success() {
-        return new R<T>().setCode(200).setMsg("success");
+        return new R<>(200, "success", null);
     }
 
+    // 静态方法创建错误的响应，包含数据
     public static <T> R<T> error(T data) {
-        return new R<T>().setCode(500).setMsg("error").setData(data);
+        return new R<>(500, "error", data);
     }
 
+    // 静态方法创建错误的空响应
     public static <T> R<T> error() {
-        return new R<T>().setCode(500).setMsg("error");
+        return new R<>(500, "error", null);
     }
 
+    // 静态方法创建指定消息的错误响应
     public static <T> R<T> error(String msg) {
-        return new R<T>().setCode(500).setMsg(msg);
+        return new R<>(500, msg, null);
     }
 
-    public static <T> R<T> error(Integer code, String msg) {
-        return new R<T>().setCode(code).setMsg(msg);
+    // 静态方法创建指定状态码和消息的错误响应
+    public static <T> R<T> error(int code, String msg) {
+        return new R<>(code, msg, null);
     }
-
-
 }
